8622ee80d8dbaf169442855a6d0adf80292786c1
Matvej Kurakin <matvejkurakin7755@gmail.com>
2024-02-14 12:50:56 +0300

    3rd

--- include/table.h
+++ include/table.h
@@ -3,45 +3,38 @@
 #include <vector>
 
 using namespace std;
-template<typename TypeKey, typename T>
-class Iterator {
-	friend class Table;
-protected:
-	pair<TypeKey, T>* iterator;
-public:
-	Iterator(pair<TypeKey, T>& data) {
-		iterator = &data;
-	}
 
-	T& operator*() const {
-		return iterator->second;
-	}
-	/* TODO: dodelac
-	T& operator->() const {
-		return *iterator;
-	}*/
-	Iterator& operator++() {
-		iterator++;
-		return *this;
-	}
-	Iterator& operator--() {
-		iterator--;
-		return *this;
-	}
-	Iterator operator+(int offset) {
-		Iterator<T> tmp = *this;
-		tmp.iterator += offset;
-	}
-};
+template<typename TypeKey, typename T>
+class Iterator;
 
 template <typename TypeKey, typename TypeData>
 class Table {
 	vector <pair<TypeKey, TypeData>> storage;
+public:
+	Table() {
+		storage = vector<pair<TypeKey, TypeData>>;
+	}
+	Table(int size) {
+		storage = vector<pair<TypeKey, TypeData>>(size);
+	}
+	
+	Table(const Table& other) {
+		storage = other.storage;
+	}
+	~Table() {
+		storage=vector<pair<TypeKey, TypeData>>(0);
+	}
 
+	Table& operator=(const Table& other) {
+		if (this != other)
+			return *this;
+		storage = other.storage;
+		return this*;
+	}
 	TypeData& operator[] (TypeKey& key) {
 		for (int i = 0; i < storage.size(); i++) {
 			if (storage[i].first == key)
-				return *storage.second;
+				return storage[i].second;
 		}
 		storage.push_back(make_pair(key, TypeData()));
 		return storage.back().second;
@@ -70,12 +63,54 @@ class Table {
 		}
 		return false;
 	}*/
-	Iterator& begin() {
-		return Iterator(storage[0]);
+	Iterator<TypeKey, TypeData>& begin() {
+		return Iterator<TypeKey, TypeData>(storage[0]);
 	}
-	Iterator& end() {
-		return begin() + storage.size();
+	Iterator<TypeKey, TypeData>& end() {
+		Iterator<TypeKey, TypeData> a = begin();
+		return a + storage.size();
 	}
 
 };
 
+template<typename TypeKey, typename T>
+class Iterator {
+	friend class Table< TypeKey, T>;
+protected:
+	pair<TypeKey, T>* iterator;
+public:
+	Iterator(pair<TypeKey, T>& data) {
+		iterator = &data;
+	}
+
+	T& operator*() const {
+		return iterator->second;
+	}
+	// TODO: dodelac
+	T& operator->() const {
+		return *iterator.second;
+	}
+	Iterator& operator++() {
+		iterator++;
+		return *this;
+	}
+	Iterator& operator--() {
+		iterator--;
+		return *this;
+	}
+	Iterator operator+(int offset) {
+		Iterator<TypeKey, T> tmp = *this;
+		tmp.iterator += offset;
+		return tmp;
+	}
+	bool operator==(const Iterator& other) {
+		if (iterator == other.iterator)
+			return true;
+		return false;
+	}
+	bool operator!=(const Iterator& other) {
+		if (iterator != other.iterator)
+			return true;
+		return false;
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 
 int main() {
+	Table<int, int> a(3);
+	Iterator<int, int> b(make_pair(0, 0));
+	int cnt = 0;
+	for (auto i = a.begin(); i != a.end(); ++i) {
+		cnt++;
+	}
+	cout << cnt;
 	return 0;
 }
