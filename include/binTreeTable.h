aa6d9fff0efc2ad033700b866e34a11b91747502
Matvej Kurakin <matvejkurakin7755@gmail.com>
2024-04-07 12:32:07 +0300

    binTreeTable without iterator

--- include/binTreeTable.h
+++ include/binTreeTable.h
@@ -6,7 +6,7 @@
 template <typename TypeKey, typename TypeData>
 class Node {
 public:
-	pair<TypeData,TypeKey> store;
+	pair<TypeData,TypeKey> storage;
 	Node* left;
 	Node* right;
 	Node* parent;
@@ -36,56 +36,250 @@ public:
 	}
 	//--------------------------------------------------------------------------------//
 	BinTreeTable(const BinTreeTable& other) { //конструктор копирования
-		if (root == 0) 
-			other.root = 0;
+		if (other.root == 0) {
+			root = 0;
+		}
 		else {
-			//не знаю как реализовать
-			//по идее мы должны бродить по дереву и занулять
-			//тут уже не выйдет, как с деструктором, здесь не получится какой то ориентир придумать
-			//1) идея: завести переменную, которая будет хранить последнее перемещение
-			//например: начальный проход это влево, если влево нельзя - значит вправо, если предыдущий был вправо, то наверх и тд
-			//и там можно поля проверять для сверки какой шаг был последний, предыдущий, как мы туда попали
-			//но решение попахивает сложной реализвацией и путаницей
-			//2) можно просто сделать новое поле, которые будет говорить - были или нет
-			//но как будто реализация не сильно облегчится, все равно ползать через цикл
-			//3)как то реализовать рекурсивную функцию, но пока что не придумал как, реализвация будет самой крутой и по идее не сложной, но я пока что не додумался(
-			
+			root = new Node<TypeKey, TypeData>();
+			root->parent = 0;
+			root->left = 0;
+			root->right = 0;
+			root->storage = other.root->storage;
+			Node<TypeKey, TypeData>* n1 = root;
+			Node<TypeKey, TypeData>* n2 = root;
+			Node<TypeKey, TypeData>* other_n1 = other.root;
+			Node<TypeKey, TypeData>* other_n2 = other.root;
+			while (n2 != 0) {
+				n1 = n2;
+				other_n1 = other_n2;
+				if (other_n1->left != 0 && n1->left == 0) {
+					other_n2 = other_n1->left;
+					n2 = new Node<TypeKey, TypeData>();
+					n2->parent = n1;
+					n2->left = 0;
+					n2->right = 0;
+					n2->storage = other_n2->storage;
+					n1->left = n2;
+				}
+				else if (other_n1->right != 0 && n1->right == 0) {
+					other_n2 = other_n1->right;
+					n2 = new Node<TypeKey, TypeData>();
+					n2->parent = n1;
+					n2->left = 0;
+					n2->right = 0;
+					n2->storage = other_n2->storage;
+					n1->right = n2;
+				}
+				else {
+					n2 = n1->parent;
+					other_n2 = other_n1->parent;
+				}
+			}
+
 		}
 	}
 	//--------------------------------------------------------------------------------//
 	~BinTreeTable() { //деструктор
-		//не знаю как реализовать
-		//по идее мы должны бродить по дереву и занулять
-		//можно сделать цикл и идти сначала в лево, если в лево есть и направо, если направо есть, при выходе удаляться и занулять у предка откуда пришел
+		if (root == 0) {
+			delete root;// = nullptr;
+		}
+		else {
+			Node<TypeKey,TypeData>* n1 = root;
+			Node<TypeKey, TypeData>* n2 = root;
+			while (n2 != NULL) {
+				n1 = n2;
+				if (n1->left != 0) {
+					n2 = n1->left;
+					n1->left = 0;
+				}
+				else if (n2->right != 0) {
+					n2 = n1->right;
+					n1->right = 0;
+				}
+				else {
+					n2 = n1->parent;
+					delete n1;
+				}
+			}
+		}
+		root = 0;
+		//delete root;
 	}
 	//--------------------------------------------------------------------------------//
-	//BinTreeTable& operator=(const Table& other) { //оператор присваивания
-	//по идее тут как с копированием, надо пописать, мне нужен перерыв, потом доделаю) или вместе сделаем, или ты сама сделаешь, короче, как выйдет
-	//}
-	////--------------------------------------------------------------------------------//
+	BinTreeTable& operator=(const BinTreeTable& other) { //оператор присваивания
+		if (&other != this) {
+			if (root == 0) {
+				if (other.root == 0) {
+					root = 0;
+				}
+				else {
+					root = new Node<TypeKey, TypeData>();
+					root->parent = 0;
+					root->left = 0;
+					root->right = 0;
+					root->storage = other.root->storage;
+					Node<TypeKey, TypeData>* n1 = root;
+					Node<TypeKey, TypeData>* n2 = root;
+					Node<TypeKey, TypeData>* other_n1 = other.root;
+					Node<TypeKey, TypeData>* other_n2 = other.root;
+					while (n2 != 0) {
+						n1 = n2;
+						other_n1 = other_n2;
+						if (other_n1->left != 0 && n1->left == 0) {
+							other_n2 = other_n1->left;
+							n2 = new Node<TypeKey, TypeData>();
+							n2->parent = n1;
+							n2->left = 0;
+							n2->right = 0;
+							n2->storage = other_n2->storage;
+							n1->left = n2;
+						}
+						else if (other_n1->right != 0 && n1->right == 0) {
+							other_n2 = other_n1->right;
+							n2 = new Node<TypeKey, TypeData>();
+							n2->parent = n1;
+							n2->left = 0;
+							n2->right = 0;
+							n2->storage = other_n2->storage;
+							n1->right = n2;
+						}
+						else {
+							n2 = n1->parent;
+							other_n2 = other_n1->parent;							
+						}
+					}
+
+				}
+			}
+			else {
+				if (other.root == 0) {
+					Node<TypeKey, TypeData>* n1 = root;
+					Node<TypeKey, TypeData>* n2 = root;
+					while (n2 != NULL) {
+						n1 = n2;
+						if (n1->left != 0) {
+							n2 = n1->left;
+							n1->left = 0;
+						}
+						else if (n2->right != 0) {
+							n2 = n1->right;
+							n1->right = 0;
+						}
+						else {
+							n2 = n1->parent;
+							delete n1;
+						}
+					}
+					delete n1;
+					delete n2;
+					root = 0;
+				}
+				else {
+					root->storage = other.root->storage;
+					Node<TypeKey, TypeData>* n1 = root;
+					Node<TypeKey, TypeData>* n2 = root;
+					Node<TypeKey, TypeData>* other_n1 = other.root;
+					Node<TypeKey, TypeData>* other_n2 = other.root;
+					while (n2 != 0) {
+						n1 = n2;
+						other_n1 = other_n2;
+						if (other_n1->left != 0 && n1->left == 0) {
+							other_n2 = other_n1->left;
+							n2 = new Node<TypeKey, TypeData>();
+							n2->parent = n1;
+							n2->left = 0;
+							n2->right = 0;
+							n2->storage = other_n2->storage;
+							n1->left = n2;
+						}
+						else if (other_n1->right != 0 && n1->right == 0) {
+							other_n2 = other_n1->right;
+							n2 = new Node<TypeKey, TypeData>();
+							n2->parent = n1;
+							n2->left = 0;
+							n2->right = 0;
+							n2->storage = other_n2->storage;
+							n1->right = n2;
+						}
+						else if (other_n1->left == 0 && n1->left != 0) {
+							Node<TypeKey, TypeData>* n_n1 = n1->left;
+							Node<TypeKey, TypeData>* n_n2 = n1->left;
+							while (n_n2 != NULL) {
+								n_n1 = n_n2;
+								if (n_n1->left != 0) {
+									n_n2 = n_n1->left;
+									n_n1->left = 0;
+								}
+								else if (n_n2->right != 0) {
+									n_n2 = n_n1->right;
+									n_n1->right = 0;
+								}
+								else {
+									n_n2 = n_n1->parent;
+									delete n_n1;
+								}
+							}
+							delete n_n1;
+							delete n_n2;
+							n1->left = 0;
+						}
+						else if (other_n1->right == 0 && n1->right != 0) {
+							Node<TypeKey, TypeData>* n_n1 = n1->right;
+							Node<TypeKey, TypeData>* n_n2 = n1->right;
+							while (n_n2 != NULL) {
+								n_n1 = n_n2;
+								if (n_n1->left != 0) {
+									n_n2 = n_n1->left;
+									n_n1->left = 0;
+								}
+								else if (n_n2->right != 0) {
+									n_n2 = n_n1->right;
+									n_n1->right = 0;
+								}
+								else {
+									n_n2 = n_n1->parent;
+									delete n_n1;
+								}
+							}
+							delete n_n1;
+							delete n_n2;
+							n1->right = 0;
+						}
+						else {
+							n2 = n1->parent;
+							other_n2 = other_n1->parent;
+						}
+					}
+				}
+			}
+		}
+		return *this;
+	}
+	//--------------------------------------------------------------------------------//
 	TypeData& operator[] (const TypeKey& key) { //оператор []
 		Node<TypeKey, TypeData>* n1 = root;
-		while (n1 != 0 && n1->store.first != key) {
-			if (n1->store.first < key)
+		while (n1 != 0 && n1->storage.first != key) {
+			if (n1->storage.first < key)
 				n1 = n1->right;
 			else
 				n1 = n1->left;
 		}
-		if (n1 == 0) throw "not_found";
-		if (n1->store.first != key)
-			return n1->store.second;
+		if (n1 == 0)
+			throw "not_found";
+		if (n1->storage.first != key)
+			return n1->storage.second;
 	}
 	//--------------------------------------------------------------------------------//
 		//возвращаемое значение - ссылка на итератор на найденный элемент
 	Iterator<TypeKey, TypeData> find(const TypeKey& key){
 		Node<TypeKey, TypeData>* n1 = root;
-		while (n1 != 0 && n1->store.first != key) {
-			if (n1->store.first < key)
+		while (n1 != 0 && n1->storage.first != key) {
+			if (n1->storage.first < key)
 				n1 = n1->right;
 			else
 				n1 = n1->left;
 		}
-		return Iterator<TypeKey, TypeData>(n1->store);
+		return Iterator<TypeKey, TypeData>(n1->storage);
 	}
 	int size() {
 
@@ -98,49 +292,49 @@ public:
 			root->parent = 0;
 			root->left = 0;
 			root->right = 0;
-			root->store = make_pair(key, d);
-			return Iterator<TypeKey, TypeData>(root->store);
+			root->storage = make_pair(key, d);
+			return Iterator<TypeKey, TypeData>(root->storage);
 		}
 		else {
 			Node<TypeKey, TypeData>* n1 = root;
 			Node<TypeKey, TypeData>* n2 = 0;
-			while (n1 != 0 && n1->store.first != key) {
+			while (n1 != 0 && n1->storage.first != key) {
 				n2 = n1;
-				if (n1->store.first < key)
+				if (n1->storage.first < key)
 					n1 = n1->right;
 				else
 					n1 = n1->left;
 			}
 			if (n1 == 0) {
 				n1 = new Node<TypeKey, TypeData>();
-				n1->store = make_pair(key, d);
+				n1->storage = make_pair(key, d);
 				n1->left = 0;
 				n1->right = 0;
 				n1->parent = n2;
-				if (n2->store.first < n1->store.first)
+				if (n2->storage.first < n1->storage.first)
 					n2->right = n1;
 				else
 					n2->left = n1;
 			}
-			else if (n1->store.first == key) {
-				n1->store.second = d;
+			else if (n1->storage.first == key) {
+				n1->storage.second = d;
 			}
-			return Iterator<TypeKey, TypeData>(n1->store);
+			return Iterator<TypeKey, TypeData>(n1->storage);
 		}
 	}
 	//--------------------------------------------------------------------------------//
 		//возвращаем true/false
 	bool remove(const TypeKey& key) {
 		Node<TypeKey, TypeData>* n1 = root;
-		while (n1 != 0 && n1->store.first != key) {
-			if (n1->store.first < key)
+		while (n1 != 0 && n1->storage.first != key) {
+			if (n1->storage.first < key)
 				n1 = n1->right;
 			else
 				n1 = n1->left;
 		}
 		if (n1 == 0)
 			return false;
-		if (n1->store.first == key) {
+		if (n1->storage.first == key) {
 			Node<TypeKey, TypeData>* nr;
 			if (n1->right != 0) {
 				nr = n1->right;
@@ -152,11 +346,13 @@ public:
 			}
 			else
 				nr = n1->left;
-			if (n1->parent == 0)
+			if (n1->parent == 0) {
+				nr->parent = 0;
 				root = nr;
+			}
 			else {
 				nr->parent = n1->parent;
-				if (n1->parent->left->store.first == key)
+				if (n1->parent->left->storage.first == key)
 					n1->parent->left = nr;
 				else
 					n1->parent->right = nr;
